954587f41071a78eed819400acb2c08ecd5961e4
Tejas <20110003@pvgcoet.ac.in>
2023-03-20 00:53:40 +0530

    Updates

---
+++ precache-manifest.0c5931def7c37ed6a8a0282fe1d3b406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "411732cf5bf19fa4e7f9",
+    "url": "/imdb-app/static/css/main.508c04cd.chunk.css"
+  },
+  {
+    "revision": "411732cf5bf19fa4e7f9",
+    "url": "/imdb-app/static/js/main.411732cf.chunk.js"
+  },
+  {
+    "revision": "5df50f20135844d1f62b",
+    "url": "/imdb-app/static/js/1.5df50f20.chunk.js"
+  },
+  {
+    "revision": "bcbfd244dd868515583a",
+    "url": "/imdb-app/static/css/2.b0e28df3.chunk.css"
+  },
+  {
+    "revision": "bcbfd244dd868515583a",
+    "url": "/imdb-app/static/js/2.bcbfd244.chunk.js"
+  },
+  {
+    "revision": "96ad690466f928afa94d",
+    "url": "/imdb-app/static/js/runtime~main.96ad6904.js"
+  },
+  {
+    "revision": "f9d5188fc2109006ad9746bf20aadbdb",
+    "url": "/imdb-app/index.html"
+  }
+];
