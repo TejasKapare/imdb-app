b812bc144c10cd83f13504da36c764b78e80f094
Tejas <20110003@pvgcoet.ac.in>
2023-03-20 00:42:10 +0530

    Updates

---
+++ precache-manifest.7afb94bb7ce7305c7744861ba04d06e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "acfd1e64ab3dcb510188",
+    "url": "/static/css/main.508c04cd.chunk.css"
+  },
+  {
+    "revision": "acfd1e64ab3dcb510188",
+    "url": "/static/js/main.acfd1e64.chunk.js"
+  },
+  {
+    "revision": "5df50f20135844d1f62b",
+    "url": "/static/js/1.5df50f20.chunk.js"
+  },
+  {
+    "revision": "bcbfd244dd868515583a",
+    "url": "/static/css/2.b0e28df3.chunk.css"
+  },
+  {
+    "revision": "bcbfd244dd868515583a",
+    "url": "/static/js/2.bcbfd244.chunk.js"
+  },
+  {
+    "revision": "7783f96e75bead0d1762",
+    "url": "/static/js/runtime~main.7783f96e.js"
+  },
+  {
+    "revision": "f423ef8ed80bf0ba57d63f913732233c",
+    "url": "/index.html"
+  }
+];
