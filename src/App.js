6f40dbd876e80da96c90c2b1524a3cb55e76a2d2
Tejas <20110003@pvgcoet.ac.in>
2023-03-20 12:46:46 +0530

    Home page update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <Route index element={<Home />}></Route>
           <Route path="movie/:id" element={<Movie />}></Route>
           <Route path="movies/:type" element={<Movielist/>}></Route>
-          <Route path="/*" element={<h1>error page</h1>}></Route>
+          <Route path="/*" element={<Home />}></Route>
         </Routes>
       </Router>
     </div>
